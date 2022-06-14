5f6b67b734d631fea071caed0df65e123eddd15d
Juan Carlos Donado Vanegas <54679777+juancDonado@users.noreply.github.com>
2022-06-14 15:44:52 -0500

    Ejercicio de sesion 3
    Actualizacion del ejercicio 1 de la session 2 y los nuevos ejercicios de la sesion 3

---
+++ EJERCICIO2_PARTE1.sql
@@ -0,0 +1,36 @@
+
+DECLARE 
+v_opinion opinion%ROWTYPE;
+v_id_opinion opinion.idopinion%TYPE;
+v_name_movie pelicula.titulo%TYPE;
+v_num NUMBER;
+v_text VARCHAR(20);
+
+BEGIN
+
+v_num := &v_id_opinion;
+
+SELECT o.idopinion, p.titulo
+INTO v_id_opinion, v_name_movie
+FROM opinion o, pelicula p
+WHERE o.idopinion = v_num AND o.idpelicula = p.idpelicula;
+
+SELECT o.*
+INTO v_opinion
+FROM opinion o
+WHERE o.idopinion = v_id_opinion;
+
+CASE 
+WHEN v_opinion.puntuacion IN (1,2) THEN
+    v_text := 'Mala';
+WHEN v_opinion.puntuacion IN (3,4) THEN
+    v_text := 'Buena';
+WHEN v_opinion.puntuacion = 5 THEN
+    v_text := 'Excelente';
+END CASE;
+
+DBMS_OUTPUT.PUT_LINE('La pelicula '||v_name_movie||' tuvo una puntuacion de: '||v_text);
+
+
+
+END;
